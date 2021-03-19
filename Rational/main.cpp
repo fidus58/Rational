79b235df54ec4373541c619c48c425c29e7aba1d
Klaus Ahrens <ahrens@informatik.hu-berlin.de>
2021-03-19 11:03:39 +0100

    Initial Commit

---
+++ Rational/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Rational
+//
+//  Created by Klaus Ahrens on 19.03.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
